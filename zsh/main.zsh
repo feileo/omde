66fb6dd3e1f9f7c7941013a579316127b234c39a
feileo <g@feileo.com>
2024-07-20 22:36:58 +0800

    update zsh and tmux config

--- zsh/main.zsh
+++ zsh/main.zsh
@@ -54,6 +54,12 @@ source $ZSH/oh-my-zsh.sh
 ############################### User settings ###############################
 setopt HIST_IGNORE_DUPS
 
+if [[ $TMUX != "" ]] then
+  export TERM="tmux-256color"
+else
+  export TERM="xterm-256color"
+fi
+
 DEFAULT_USER=$USER
 
 source $MDE/zsh/cmd.zsh
